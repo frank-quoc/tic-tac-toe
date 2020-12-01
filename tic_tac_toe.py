895f9d1da453c3d56f44339666f4740cbcce56ee
frank-quoc <frankquocho214@gmail.com>
2020-12-01 12:51:40 -0800

    finished game

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,20 +3,20 @@ import time
 
 def instructions():
     """Displays how to to play the game and the rules."""
-    print('''This is a simple game of tic-tac-toe. You will be playing the game against a computer. 
+    print('''WELCOME TO TIC-TAC-TOE! You will be playing the game against a computer. 
 Moves can be made by inputting the numbers 1-9 and are representing each space in the board like this:
-                                        7|8|9
-                                        -+-+-
-                                        4|5|6
-                                        -+-+-
-                                        1|2|3 
+7|8|9
+-+-+-
+4|5|6
+-+-+-
+1|2|3 
 The games is over when the either the player or computer gets 3 in a row, column, or diagonally.\n''')
 
 def new_board():
     """Sets the new board."""
     board = {   7: ' ', 8: ' ', 9: ' ',
-            4: ' ', 5: ' ', 6: ' ',
-            1: ' ', 2: ' ', 3: ' '  }
+                4: ' ', 5: ' ', 6: ' ',
+                1: ' ', 2: ' ', 3: ' '  }
     return board
 
 def print_board(board):
@@ -55,6 +55,7 @@ def pick_first_player(human, computer):
         return computer
 
 def human_move(board):
+    """Receives input from the player to input into the square."""
     while True:
         player_move = int(input("Pick a number between 1-9 to place your move on the board: "))
         if player_move not in range(1, 10):
@@ -67,15 +68,36 @@ def human_move(board):
             print("Player turn...")
             return player_move
 
-def comp_move(board):
+def comp_move(board, computer, human):
+    """Instructions for the computer to fill in a square."""
+    print("Computer turn...")
+    time.sleep(1.5)
+    # Searches for the best available move for the computer to win.
+    for k, v in board.items():
+        if v != " ":
+            continue
+        hold_val = board[k]
+        board[k] = computer
+        if winner(board) == computer:
+            comp_move = k
+            return comp_move
+        board[k] = hold_val # if the choices didn't result in a computer win  
+    # Searches for the best available move to block the player from winning.
+    for k, v in board.items():
+        if v != " ":
+            continue
+        hold_val = board[k]
+        board[k] = human
+        if winner(board) == human:
+            comp_move = k
+            return comp_move
+        board[k] = hold_val
+    # Otherwise, just pick a random square.
     while True:
         comp_move = random.randint(1, 9)
-        if board[comp_move] != ' ':
+        if board[comp_move] != " ":
             continue
-        else:
-            print("Computer turn...")
-            time.sleep(1.5)
-            return comp_move
+        return comp_move
 
 def winner(board):
     """Checks win conditions."""
@@ -83,10 +105,17 @@ def winner(board):
                 (7, 4, 1), (8, 5, 2), (9, 6, 3), # Vertical Wins
                 (7, 5, 3), (1, 5, 9)) # Diagonal Wins
     for i in WIN_COND:
-        if board[i[0]] == board[i[1]] == board[i[2]] != ' ':
+        if board[i[0]] == board[i[1]] == board[i[2]] != " ":
             return board[i[0]]
     else: 
         return False
+
+def restart():
+    again = input("\nWould you like to start again? (y/n) ").lower()
+    if again != 'y':
+        exit()
+    else:
+        main()
 
 def main():
     moves = 0
@@ -100,19 +129,23 @@ def main():
             board[human_move(board)] = human
             curr_player = computer
             print_board(board)
+            # Check to see if there's a winner after 5 moves.
+            if moves >= 5:
+                if winner(board) == human:
+                    print("GAME OVER! You have bested me this time, Player.")
+                    break
         else:
-            board[comp_move(board)] = computer
+            board[comp_move(board, computer, human)] = computer
             curr_player = human
             print_board(board)
-        # Check to see if there are any winners after 5 moves.
-        if moves >= 5:
-            if winner(board) == human:
-                print("GAME OVER! Player wins the game.")
-                break
-            elif winner(board) == computer:
-                print("MWHAHAHA! Computers have taken over the world.")
-                break
+            if moves >= 5:
+                if winner(board) == computer:
+                    print("MWHAHAHA! Computers have taken over the world.")
+                    break
         moves += 1
+    if moves == 9:
+        print("TIE! We are of equal intelligence.")
+    restart()
 
 if __name__ == '__main__':
     main()
