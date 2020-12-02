9f34aa799057f090016da9a4621ead412fc562df
Frank Ho <58057620+frank-quoc@users.noreply.github.com>
2020-12-02 12:09:20 -0800

    Changed loop common variables to named variables

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -59,25 +59,25 @@ def comp_move(board, computer, human):
     print("Computer turn...")
     time.sleep(1.5)
     # Searches for the best available move for the computer to win.
-    for k, v in board.items():
-        if v != " ":
+    for num, sq_val in board.items():
+        if sq_val != " ": # If the current square is empty
             continue
-        hold_val = board[k]
-        board[k] = computer
+        hold_val = sq_val
+        board[num] = computer
         if winner(board) == computer:
-            comp_move = k
+            comp_move = num
             return comp_move
-        board[k] = hold_val # if the choices didn't result in a computer win  
+        board[num] = hold_val # if the choices didn't result in a computer win  
     # Searches for the best available move to block the player from winning.
-    for k, v in board.items():
-        if v != " ":
+    for num, sq_val in board.items():
+        if sq_val != " ":
             continue
-        hold_val = board[k]
-        board[k] = human
+        hold_val = sq_val
+        board[num] = human
         if winner(board) == human:
-            comp_move = k
+            comp_move = num
             return comp_move
-        board[k] = hold_val
+        board[num] = hold_val
     # Otherwise, just pick a random square.
     while True:
         comp_move = randint(1, 9)
@@ -90,13 +90,14 @@ def winner(board):
     WIN_COND = ((7, 8, 9), (4, 5, 6), (1, 2 ,3), # Horizontal Wins
                 (7, 4, 1), (8, 5, 2), (9, 6, 3), # Vertical Wins
                 (7, 5, 3), (1, 5, 9)) # Diagonal Wins
-    for i in WIN_COND:
-        if board[i[0]] == board[i[1]] == board[i[2]] != " ":
-            return board[i[0]]
+    for square in WIN_COND:
+        if board[square[0]] == board[square[1]] == board[square[2]] != " ":
+            return board[square[0]]
     else: 
         return False
 
 def restart():
+    """Asks the player if they want to play again."""
     again = input("\nWould you like to start again? (y/n) ").lower()
     print()
     if again != 'y':
@@ -113,24 +114,24 @@ def main():
 
     while moves < 9:
         if curr_player == human:
-            board[human_move(board)] = human
-            curr_player = computer
+            board[human_move(board)] = human # Set the board[human_num] to the human pick of X or O
+            curr_player = computer # Switch to the other player
             print_board(board)
             # Check to see if there's a winner after 5 moves.
-            if moves >= 5:
+            if moves >= 5: # After 5 moves, check if there's a winner
                 if winner(board) == human:
                     print("GAME OVER! You have bested me this time, Player.")
                     break
         else:
-            board[comp_move(board, computer, human)] = computer
-            curr_player = human
+            board[comp_move(board, computer, human)] = computer # Set the board[comp_num] to the opposite of the human pick of X or O
+            curr_player = human # Switch to the other player
             print_board(board)
-            if moves >= 5:
+            if moves >= 5: # After 5 moves, check if there's a winner
                 if winner(board) == computer:
                     print("MWHAHAHA! Computers have taken over the world.")
                     break
         moves += 1
-    if moves == 9:
+    if moves == 9: # If there's no winner at 9 moves, it's a tie
         print("TIE! We are of equal intelligence.")
     restart()
 
